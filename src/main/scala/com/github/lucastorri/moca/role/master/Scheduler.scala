3abfde7457cc3537a7ac8a1fe76d7d42dab16ae1
Lucas Torri <lucastorri@gmail.com>
2015-09-03 20:16:05 +0200

    organize scheduler code

--- src/main/scala/com/github/lucastorri/moca/role/master/Scheduler.scala
+++ src/main/scala/com/github/lucastorri/moca/role/master/Scheduler.scala
@@ -1,6 +1,7 @@
 package com.github.lucastorri.moca.role.master
 
-import akka.actor.{Props, ActorRefFactory, ActorRef, Status}
+import akka.actor.{ActorRefFactory, Props, Status}
+import akka.pattern.ask
 import akka.persistence.{PersistentActor, RecoveryCompleted}
 import akka.util.Timeout
 import com.github.lucastorri.moca.role.Task
@@ -12,8 +13,6 @@ import com.typesafe.scalalogging.StrictLogging
 import scala.collection.mutable
 import scala.concurrent.Future
 import scala.concurrent.duration._
-import akka.pattern.ask
-
 
 case class Scheduler(actorFactory: ActorRefFactory) {
 
@@ -57,47 +56,6 @@ class SchedulerActor extends PersistentActor with StrictLogging {
   private val taskPartition = mutable.HashMap.empty[String, String]
   private val scheduled = mutable.ListBuffer.empty[Task]
 
-  private def add(task: Task): Unit = {
-    if (lockedPartitions.contains(task.partition)) {
-      val queue = partitionQueues.getOrElseUpdate(task.partition, mutable.ListBuffer.empty)
-      queue.append(task)
-    } else {
-      lockedPartitions.add(task.partition)
-      scheduled.append(task)
-    }
-  }
-
-  private def next(): Option[Task] = {
-    val next = scheduled.headOption
-    next.foreach { task =>
-      scheduled.remove(0)
-      taskPartition.put(task.id, task.partition)
-    }
-    next
-  }
-
-  private def remove(taskId: String): Unit = {
-    val partition = taskPartition(taskId)
-    partitionQueues.get(partition) match {
-      case Some(queue) =>
-        scheduled.append(queue.remove(0))
-        if (queue.isEmpty) partitionQueues.remove(partition)
-      case None =>
-        lockedPartitions.remove(partition)
-    }
-  }
-
-  override protected def onPersistFailure(cause: Throwable, event: Any, seqNr: Long): Unit = {
-    logger.error("Persist failure", cause)
-    sender() ! Status.Failure(cause)
-  }
-
-
-  override protected def onPersistRejected(cause: Throwable, event: Any, seqNr: Long): Unit = {
-    logger.error("Persist rejected", cause)
-    sender() ! Status.Failure(cause)
-  }
-
   override def receiveRecover: Receive = {
 
     case e: Event =>
@@ -117,14 +75,44 @@ class SchedulerActor extends PersistentActor with StrictLogging {
   def handle: Function[Event, Any] = {
 
     case Add(task) =>
-      add(task)
+      if (lockedPartitions.contains(task.partition)) {
+        val queue = partitionQueues.getOrElseUpdate(task.partition, mutable.ListBuffer.empty)
+        queue.append(task)
+      } else {
+        lockedPartitions.add(task.partition)
+        scheduled.append(task)
+      }
 
     case Next =>
-      next()
+      val next = scheduled.headOption
+      next.foreach { task =>
+        scheduled.remove(0)
+        taskPartition.put(task.id, task.partition)
+      }
+      next
 
     case Release(taskIds @ _*) =>
-      taskIds.foreach(taskId => remove(taskId))
+      taskIds.foreach { taskId =>
+        val partition = taskPartition(taskId)
+        partitionQueues.get(partition) match {
+          case Some(queue) =>
+            scheduled.append(queue.remove(0))
+            if (queue.isEmpty) partitionQueues.remove(partition)
+          case None =>
+            lockedPartitions.remove(partition)
+        }
+      }
 
+  }
+
+  override protected def onPersistFailure(cause: Throwable, event: Any, seqNr: Long): Unit = {
+    logger.error("Persist failure", cause)
+    sender() ! Status.Failure(cause)
+  }
+
+  override protected def onPersistRejected(cause: Throwable, event: Any, seqNr: Long): Unit = {
+    logger.error("Persist rejected", cause)
+    sender() ! Status.Failure(cause)
   }
 
   override def persistenceId: String = "task-scheduler"
