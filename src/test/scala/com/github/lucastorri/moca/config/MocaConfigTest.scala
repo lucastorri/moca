f39f3717d96bdc2ad3e7d2587b0acc6c4e2806b5
Lucas Torri <lucastorri@gmail.com>
2015-09-27 11:55:04 +0200

    add tests for mocaconfig resolution

---
+++ src/test/scala/com/github/lucastorri/moca/config/MocaConfigTest.scala
@@ -0,0 +1,24 @@
+package com.github.lucastorri.moca.config
+
+import org.scalatest.{FlatSpec, MustMatchers}
+
+import scala.collection.JavaConversions._
+
+class MocaConfigTest extends FlatSpec with MustMatchers {
+
+  it must "resolve pending configurations" in {
+
+    val port = 7890
+    val hostname = "10.11.12.13"
+    val seeds = Set("seed1:9123", "seed2:9234")
+
+    val config = MocaConfig()
+      .copy(seeds = seeds, hostname = hostname, port = port)
+
+    config.mainConfig.getInt("akka.remote.netty.tcp.port") must equal (port)
+    config.mainConfig.getString("akka.remote.netty.tcp.hostname") must equal (hostname)
+    config.mainConfig.getStringList("akka.cluster.seed-nodes").toSet must equal (seeds)
+
+  }
+
+}
